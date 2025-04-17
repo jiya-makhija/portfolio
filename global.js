c23362f9332dab9191033403cc5c728c1b8548e8
Jiya Makhija <jmakhija@ucsd.edu>
2025-04-17 14:12:46 -0700

    adding nav menu using js

--- global.js
+++ global.js
@@ -10,3 +10,20 @@ let currentLink = navLinks.find(
   (a) => a.host === location.host && a.pathname === location.pathname
 );
 currentLink?.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/jiya-makhija', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
