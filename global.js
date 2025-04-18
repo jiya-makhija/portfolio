56708b6875eec1b16d34578ed133c15ab446c59a
Jiya Makhija <jmakhija@ucsd.edu>
2025-04-18 13:42:19 -0700

    Update global.js

--- global.js
+++ global.js
@@ -70,7 +70,14 @@ document.body.insertAdjacentHTML(
 const select = document.querySelector('.color-scheme select');
 
 select.addEventListener('input', function (event) {
-  const selected = event.target.value;
-  console.log('color scheme changed to', selected);
-  document.documentElement.style.setProperty('color-scheme', selected);
+  const value = event.target.value;
+  console.log("color scheme changed to:", value);
+
+  document.documentElement.classList.remove('light', 'dark', 'auto');
+
+  if (value === "light dark") {
+    document.documentElement.classList.add('auto');
+  } else {
+    document.documentElement.classList.add(value); // "light" or "dark"
+  }
 });
