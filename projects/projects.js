a6b292ae7b4225862e9c848eedf4d7ac65852f6c
Jiya Makhija <jmakhija@ucsd.edu>
2025-04-30 13:10:20 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,15 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
+
 const projects = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
 
 const title = document.querySelector('.projects-title');
 if (title) title.textContent = `${projects.length} Projects`;
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
