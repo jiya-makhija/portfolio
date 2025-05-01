9868a20723c69753031f75b65bf3a631128184a8
Jiya Makhija <jmakhija@ucsd.edu>
2025-04-30 19:35:15 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-
 const projects = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
@@ -9,7 +8,18 @@ const title = document.querySelector('.projects-title');
 if (title) title.textContent = `${projects.length} Projects`;
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-let arc = d3.arc().innerRadius(0).outerRadius(50)({
-    startAngle: 0,
-    endAngle: 2 * Math.PI,
-  });
+
+let data = [1, 2, 3, 4, 5, 5];
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcData.forEach((d, i) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arcGenerator(d))
+    .attr('fill', colors(i));
+});
