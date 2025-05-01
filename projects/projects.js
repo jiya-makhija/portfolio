3298ff12fdcbbbc53b903a4e7932fd789dbd75db
Jiya Makhija <jmakhija@ucsd.edu>
2025-04-30 19:37:53 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -9,12 +9,19 @@ if (title) title.textContent = `${projects.length} Projects`;
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 arcData.forEach((d, i) => {
