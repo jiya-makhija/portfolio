9a8a8415405c8eb84c872ec4184477d5ac4c6abf
Jiya Makhija <jmakhija@ucsd.edu>
2025-05-01 16:38:15 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -9,14 +9,15 @@ if (title) title.textContent = `${projects.length} Projects`;
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
