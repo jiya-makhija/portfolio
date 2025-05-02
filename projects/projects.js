117e3a2c0c634fc0fac8b86141d7599e6f089ebd
Jiya Makhija <jmakhija@ucsd.edu>
2025-05-01 17:45:14 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,7 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-const container = document.querySelector('.projects');
+let projectsContainer = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
 
 const title = document.querySelector('.projects-title');
